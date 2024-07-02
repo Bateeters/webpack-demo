ff63a38391765afa04346779e6e0905423fa52e1
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-07-02 07:22:25 -0400

    Added font files to webpack bundling

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function component() {
     const element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack,'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
     // Add the image to our existing div.
