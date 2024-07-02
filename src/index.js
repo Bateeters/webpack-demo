ebe2f2bc66450b01fbb8e3ef2ff8020fc154b1b1
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-07-02 07:47:21 -0400

    Added Data Files to webpack bundling
    toml, yaml, json5 files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
 
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `John Doe`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `John Doe`
 
 function component() {
     const element = document.createElement('div');
@@ -15,6 +28,9 @@ function component() {
     myIcon.src = Icon;
 
     element.appendChild(myIcon);
+
+    console.log(Data);
+    console.log(Notes);
   
     return element;
   }
